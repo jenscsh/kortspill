ce4ea60a0da71d99d80d0d6a44786e2fe3ffab6e
jenscsh <jenschrsh@gmail.com>
2020-11-23 05:10:26 +0100

    Added stuff

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 let cards = document.querySelectorAll(".card");
-let cardToFollow;
+let hand = document.querySelector("#hand");
+let board = document.querySelector("#board");
+let chosenCard;
 cards.forEach(card => {
-    card.addEventListener("mousedown", ()=> {
-    cardToFollow=card;
-    console.log("Mouse down: "+cardToFollow);
+    card.addEventListener("click",()=>{
+        if (card===chosenCard) Unchoose();
+        else {
+            Unchoose();
+            chosenCard=card;
+            card.style.border = "5px orange solid";
+            card.querySelector("button").style.display = "block";
+        }
     });
-    card.addEventListener("touchstart", ()=> {
-        cardToFollow=card;
-        console.log("Touch down: "+cardToFollow);
+    card.querySelector("button").addEventListener("click",(e)=>{
+        let turned = false;
+        if (turned) {
+            card.style.transform = "rotateY(0deg)";
+            e.target.style.transform = "rotateY(0deg)";
+        } else {
+            card.style.transform += "rotateY(180deg)";
+            e.target.style.transform += "rotateY(180deg)";
+        }
     });
 });
-document.addEventListener("mousemove", (e)=>{
-    if (cardToFollow!=null) {
-        cardToFollow.style.left = e.pageX+'px';
-        cardToFollow.style.top = e.pageY+'px';
-        cardToFollow.style.transform = "translate(-50%,-50%)";
-        cardToFollow.style.position = "absolute";
-        console.log("Mouse move: "+cardToFollow);
+document.addEventListener("click",(e)=>{
+    if (chosenCard&&e.target.id==='board') {
+        board.appendChild(chosenCard);
+        chosenCard.style.position="absolute";
+        chosenCard.style.left = e.pageX+'px';
+        chosenCard.style.top = e.pageY+'px';
+        // var topDiff = chosenCard.offsetTop - e.pageY;
+        // var leftDiff = chosenCard.offsetLeft - e.pageX;
+        // chosenCard.style.transform = `translate(${leftDiff},${topDiff})`;
+        chosenCard.style.transform = "translate(-50%,-50%)";
+        Unchoose();
+    } else if (chosenCard&&(e.target.id==='hand'/*||e.target.parentElement.id==='hand'*/)) {
+        hand.appendChild(chosenCard);
+        chosenCard.style.position="static";
+        chosenCard.style.transform = "translate(0,0)";
+        Unchoose();
     }
+    
 });
-document.addEventListener("touchmove", (e)=>{
-    if (cardToFollow!=null) {
-        cardToFollow.style.left = e.pageX+'px';
-        cardToFollow.style.top = e.pageY+'px';
-        cardToFollow.style.transform = "translate(-50%,-50%)";
-        cardToFollow.style.position = "absolute";
-        console.log("Touch move: "+cardToFollow);
+function Unchoose() {
+    if (chosenCard) {
+        chosenCard.style.border="none";
+        chosenCard.querySelector("button").style.display = "none";
     }
-});
-document.addEventListener("mouseup",()=> {
-    console.log("Mouse up: "+cardToFollow);
-    cardToFollow=null;
-});
-document.addEventListener("touchend",()=> {
-    console.log("Touch up: "+cardToFollow);
-    cardToFollow=null;
-});
+    chosenCard=null;
+}
